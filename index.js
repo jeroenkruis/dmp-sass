00a79312c2bf1d3d4d9cbebd52cb217f800b82ed
Jeroen Kruis <kruisjeroen@gmail.com>
2015-02-19 22:44:06 +0100

    Edit extension from .scss to .sass

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var fs = require('fs');
-var path = require('path');
-var sass = require('node-sass');
+var fs      = require('fs');
+var path    = require('path');
+var sass    = require('node-sass');
 
 module.exports = function dmpSass ($, document, done) {
   sass.render({
-    file: 'assets/css/style.scss',
+    file: 'assets/css/style.sass',
     outputStyle: 'compressed',
     sourceMap: false,
     success: function (result) {
       var cache = require('documark-cache')(document);
       var file = cache.fileWriteStream('sass-cache.css');
 
-      file.end(result.css);
       document.config().pdf.userStyleSheet = 'file://' + cache.filePath('sass-cache.css');
-    },
+
+      file.end(result.css);
+      },
     error: function (error) {
       console.log(error.message);
     }

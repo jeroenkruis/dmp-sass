db9fd5513c9a6f8368b3bac40e80243d614f35aa
Jeroen Kruis <kruisjeroen@gmail.com>
2015-02-19 21:01:49 +0100

    First setup

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ var path = require('path');
 var sass = require('node-sass');
 
 module.exports = function documark-sass ($, document, done) {
+  sass.renderSync({
+    file: 'assets/css/style.scss',
+    outputStyle: 'compressed',
+    outFile: 'assets/css/style.css',
+    sourceMap: false
+  });
+
   done();
 };

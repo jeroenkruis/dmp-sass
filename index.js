41a7b82f93bd3a18d5de1e153c315597dda90ca9
Jeroen Kruis <kruisjeroen@gmail.com>
2015-02-19 23:30:29 +0100

    Changed cache file name

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ module.exports = function dmpSass ($, document, done) {
     sourceMap: false,
     success: function (result) {
       var cache = require('documark-cache')(document);
-      var file = cache.fileWriteStream('sass-cache.css');
+      var file = cache.fileWriteStream('sass-cache-compiled.css');
 
-      document.config().pdf.userStyleSheet = 'file://' + cache.filePath('sass-cache.css');
-
+      document.config().pdf.userStyleSheet = 'file://' + cache.filePath('sass-cache-compiled.css');
       file.end(result.css);
     },
     error: function (error) {

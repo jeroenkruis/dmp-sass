c8e651cc69b547eb97f9662e6c88867d6747844b
Jeroen Kruis <kruisjeroen@gmail.com>
2015-02-19 21:22:18 +0100

    Edit name in package.json and change function name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var sass = require('node-sass');
 
-module.exports = function documark-sass ($, document, done) {
+module.exports = function dmpSass ($, document, done) {
   sass.renderSync({
     file: 'assets/css/style.scss',
     outputStyle: 'compressed',
